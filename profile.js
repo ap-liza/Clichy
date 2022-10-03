d58fb674727b6dd4888a353c15fce18996116657
Liza <100815059+ap-liza@users.noreply.github.com>
2022-10-03 02:20:02 -0700

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,16 @@
+let LOGIN = document.getElementById("loGin")
+let REGISTER = document.getElementById("reGister")
+
+
+function register(){
+    REGISTER.style.transform = "translateX(0px)"
+    LOGIN.style.transform = "translateX(0px)"
+    
+
+}
+function login(){
+    REGISTER.style.transform = "translateX(300px)"
+    LOGIN.style.transform = "translateX(300px)"
+    
+
+}
